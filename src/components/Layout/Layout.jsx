39c96835000abee27f3a25025f2d88dfa1855f8f
James Davey <james.t.davey@gmail.com>
2018-08-24 10:51:29 -0700

    add toolbar and adjust layout styles to compensate

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,10 +1,11 @@
 import React, { Fragment } from "react";
-
 import classes from "./Layout.module.css";
+
+import Toolbar from "./../Navigation/Toolbar/Toolbar";
 
 const layout = props => (
   <Fragment>
-    <div>Toolbar, Side Drawer, Backdrop</div>
+    <Toolbar />
     <main className={classes.Content}>{props.children}</main>
   </Fragment>
 );
